e923cf75bdd76091195295c4ddba73386859ff6d
vigneshmurthy <vigneshmurthyblr@gmail.com>
2014-01-28 20:25:45 +0530

    Added additional checks for Date code

--- hook/src/com/varian/hook/ValidateSfcForTR.java
+++ hook/src/com/varian/hook/ValidateSfcForTR.java
@@ -45,6 +45,7 @@ public class ValidateSfcForTR implements ActivityInterface<CompleteHookDTO> {
 		ObjectReference sfcreference = new ObjectReference(sfcRef);
 		SfcKeyData sfckeydata = sfcStateService.findSfcDataByRef(sfcreference);
 		String sfcNumber =  sfckeydata.getSfc();	
+
 		String prefix = sfcNumber.substring(0, Math.min(sfcNumber.length(), 3));
 		
 		int count = 0;
@@ -62,13 +63,51 @@ public class ValidateSfcForTR implements ActivityInterface<CompleteHookDTO> {
 		if (count == 0) {						
 			throw new InvalidSerialFormatException(20107,sfcNumber);		
 		} else {
-			if (prefix.equals("TR-")){	
+			if (prefix.equals("TR-") || prefix.length()<3){	
 				throw new InvalidSerialFormatException(20109,sfcNumber);	
+			} else {
+				String onetwo = prefix.substring(0,2);
+		        boolean hasNonAlpha = onetwo.matches("^.*[^a-zA-Z0-9 ].*$");	
+		       
+		         char first = prefix.charAt(0); 
+		         char second = prefix.charAt(1); 
+	             String threechar = String.valueOf(prefix.charAt(2));	
+	           
+		          String firsttype = null;
+		          String secondtype = null;
+		          String serialFormat = null;
+	             
+		          if(first>=48 && first<=57){
+		            firsttype= "digit";
+		          } else {
+		            firsttype = "alphabet";		          
+		          }
+		          if(second>=48 && second<=57){
+		            secondtype= "digit";
+		          } else {
+		          secondtype = "alphabet";
+		          }
+		        
+		        if ((firsttype.equals("alphabet") && secondtype.equals("digit")) || (firsttype.equals("digit") && secondtype.equals("alphabet"))){
+		            serialFormat = "Valid";
+		        } else if ((firsttype.equals("alphabet") && secondtype.equals("alphabet")) || (firsttype.equals("digit") && secondtype.equals("digit"))) {
+		        	serialFormat = "FormatError";
+		        }	            
+	            if (serialFormat.equals("FormatError")){
+	            	throw new InvalidSerialFormatException(20110,sfcNumber);  
+	            }
+	            if (hasNonAlpha == true){
+		        	throw new InvalidSerialFormatException(20111,sfcNumber);	
+		        }
+	            if (!threechar.equals("-")){
+	            	throw new InvalidSerialFormatException(20112,sfcNumber);  
+	             }
 			}
+			
 		}
 		
 	}
-	
+
 	private void initServices(){
 	//	operationConfigurationService = Services.getService(COM_SAP_ME_PRODUCTDEFINITION,OPERATION_CONFIGURATION_SERVICE);
 		sfcStateService = Services.getService(COM_SAP_ME_PRODUCTION,SFC_STATE_SERVICE);
