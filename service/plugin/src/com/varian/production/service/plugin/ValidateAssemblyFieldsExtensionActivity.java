62265eeed3bf578436211d8fafcce34af41ff045
vigneshmurthy <vigneshmurthyblr@gmail.com>
2014-02-10 14:26:49 +0530

    Added system rule for defect 524

--- service/plugin/src/com/varian/production/service/plugin/ValidateAssemblyFieldsExtensionActivity.java
+++ service/plugin/src/com/varian/production/service/plugin/ValidateAssemblyFieldsExtensionActivity.java
@@ -1,5 +1,8 @@
 package com.varian.production.service.plugin;
 import java.util.List;
+
+import com.sap.me.appconfig.FindSystemRuleSettingRequest;
+import com.sap.me.appconfig.SystemRuleSetting;
 import com.sap.me.common.ObjectReference;
 import com.sap.me.production.AssemblyDataField;
 import com.sap.me.production.AssemblyDataValidationRequest;
@@ -8,18 +11,29 @@ import com.varian.hook.exception.InvalidGlassSerialNumberException;
 import com.visiprise.frame.service.ext.ActivityInterface;
 import com.sap.me.extension.Services;
 import com.sap.me.production.SfcStateServiceInterface;
+import com.sap.me.appconfig.SystemRuleServiceInterface;
 
 public class ValidateAssemblyFieldsExtensionActivity implements
 		ActivityInterface<AssemblyDataValidationRequest> {
 
+	private static final String COM_SAP_ME_APPCONFIG = "com.sap.me.appconfig";
+	private static final String SYSTEM_RULE_SERVICE = "SystemRuleService";
 	private static final String SFC_STATE_SERVICE = "SfcStateService";
 	private static final String COM_SAP_ME_PRODUCTION = "com.sap.me.production";
 	private static final long serialVersionUID = 1L;
 	private SfcStateServiceInterface sfcStateService;
+	private SystemRuleServiceInterface systemRuleService;
 	
 	public void execute(com.sap.me.production.AssemblyDataValidationRequest dto)
 			throws Exception {
 		initServices();
+		boolean sysruleval = false;
+		String ruleName = "Z_ASSEMBLY_DATA_VALIDATION";
+		FindSystemRuleSettingRequest findsysrulereq = new FindSystemRuleSettingRequest();
+		findsysrulereq.setRuleName(ruleName);
+		SystemRuleSetting sysrulesetting = systemRuleService.findSystemRuleSetting(findsysrulereq);
+		sysruleval =  Boolean.valueOf(sysrulesetting.getSetting().toString());	
+		if (sysruleval){
 		String assemblyData = null;
 		String glassSerial = null;
 		ObjectReference objsfcRef = new ObjectReference(dto.getSfcRef().toString());
@@ -49,8 +63,13 @@ public class ValidateAssemblyFieldsExtensionActivity implements
 			}
 		}
 		}
-	
+		}
+
+	/**
+	 *	Initialization of services that are represented as fields.
+	 */
 	private void initServices(){
 		sfcStateService = Services.getService(COM_SAP_ME_PRODUCTION,SFC_STATE_SERVICE);
+		systemRuleService = Services.getService(COM_SAP_ME_APPCONFIG,SYSTEM_RULE_SERVICE);
 	}
 	}
