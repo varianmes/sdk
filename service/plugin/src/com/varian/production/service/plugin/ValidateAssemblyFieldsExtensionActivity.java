179410aff41463fcd07e50e5061e4e574ddd7ec7
vigneshmurthy <vigneshmurthyblr@gmail.com>
2014-03-11 13:47:58 +0530

    Service plugin new logic

--- service/plugin/src/com/varian/production/service/plugin/ValidateAssemblyFieldsExtensionActivity.java
+++ service/plugin/src/com/varian/production/service/plugin/ValidateAssemblyFieldsExtensionActivity.java
@@ -12,7 +12,9 @@ import com.visiprise.frame.service.ext.ActivityInterface;
 import com.sap.me.extension.Services;
 import com.sap.me.production.SfcStateServiceInterface;
 import com.sap.me.appconfig.SystemRuleServiceInterface;
-
+/*
+ Assembly validation defect 524 vmurthy
+ */
 public class ValidateAssemblyFieldsExtensionActivity implements
 		ActivityInterface<AssemblyDataValidationRequest> {
 
@@ -34,52 +36,40 @@ public class ValidateAssemblyFieldsExtensionActivity implements
 		SystemRuleSetting sysrulesetting = systemRuleService.findSystemRuleSetting(findsysrulereq);
 		sysruleval =  Boolean.valueOf(sysrulesetting.getSetting().toString());	
 		if (sysruleval){
-	    //fix
+	    //fix for auto assy defect 578
 		List<AssemblyDataField> datafield = new ArrayList<AssemblyDataField>();
 		datafield = dto.getAssemblyDataFields();
-		if (datafield.size() == 0){	
+		if (datafield.size() == 0 ){	
 			return;
 		}
 		//fix	
-		String sfcSerial = null;	
-		String assemblyData = null;
-		String lastthree = null;
-		String lastpiece = null;
-		String split[] = null;
-		String sfcPrefix =null;
-		int secondlength = 0;
-		ObjectReference objsfcRef = new ObjectReference(dto.getSfcRef().toString());
-		SfcKeyData sfcKeyData = sfcStateService.findSfcKeyDataByRef(objsfcRef);
-		String sfcNumber = sfcKeyData.getSfc(); 
-		try{
-		lastthree = sfcNumber.substring((sfcNumber.length()-4),sfcNumber.length());
-     	lastpiece = null;        
-        split = sfcNumber.split("-");
-        sfcPrefix = split[0];
-        secondlength = split[1].length();
-		} catch (Exception e) {
-			throw new InvalidGlassSerialNumberException(20119, sfcNumber);
-		}
-        if( (split.length>1) && (secondlength>3)){        
-        	if (!lastthree.contains("-")){
-        		sfcSerial = sfcNumber.substring(3,sfcNumber.length());
-            } else {
-            lastpiece = sfcNumber.substring((sfcNumber.lastIndexOf("-")+1),sfcNumber.length());
-         		if (lastpiece.length() > 2){
-         			throw new InvalidGlassSerialNumberException(20124, sfcNumber);
-         		} else if (lastpiece.length() == 0){
-   	        		throw new InvalidGlassSerialNumberException(20122, sfcNumber);
-   	        	} else {
-         			sfcSerial =  sfcNumber.substring(3,sfcNumber.lastIndexOf("-"));
-         		}
-            }
-        } else {
-        	throw new InvalidGlassSerialNumberException(20122, sfcNumber);
-        }
-     	
-		
 		List<AssemblyDataField> assemblyDataFields = dto.getAssemblyDataFields();
 		for (AssemblyDataField assemblyDataField : assemblyDataFields) {
+		if ("GLASS_SERIAL_NUMBER".equals(assemblyDataField.getAttribute()) || "SFC".equals(assemblyDataField.getAttribute())){
+			String sfcSerial = null;	
+			String assemblyData = null;
+			String lastthree = null;
+			String split[] = null;
+			int secondlength = 0;
+			ObjectReference objsfcRef = new ObjectReference(dto.getSfcRef().toString());
+			SfcKeyData sfcKeyData = sfcStateService.findSfcKeyDataByRef(objsfcRef);
+			String sfcNumber = sfcKeyData.getSfc(); 
+			try{
+			lastthree = sfcNumber.substring((sfcNumber.length()-3),sfcNumber.length());  
+	        split = sfcNumber.split("-");
+	        secondlength = split[1].length();
+			} catch (Exception e) {
+				throw new InvalidGlassSerialNumberException(20119, sfcNumber);
+			}
+	        if( (split.length>1) && (secondlength>3)){        
+	        	if (lastthree.startsWith("-") || !lastthree.contains("-")){
+	        		sfcSerial = sfcNumber.substring(3,sfcNumber.length());
+	            } else {
+	         		sfcSerial =  sfcNumber.substring(3,sfcNumber.lastIndexOf("-"));         		
+	            }
+	        } else {
+	        	throw new InvalidGlassSerialNumberException(20122, sfcNumber);
+	        }
 			if ("GLASS_SERIAL_NUMBER".equals(assemblyDataField.getAttribute())) {
 				assemblyData = assemblyDataField.getValue();
 					if (assemblyData == null || sfcSerial == null){
@@ -92,47 +82,20 @@ public class ValidateAssemblyFieldsExtensionActivity implements
 		if ("SFC".equals(assemblyDataField.getAttribute())) {
 			String csfc = assemblyDataField.getValue();
 			String clastthree  =null;
-			 String clastpiece = null;
 			 String csplit[] = null;
-			 String assemblyDataPrefix = null;
 			 int seclength = 0;
 			 try {
-			clastthree = csfc.substring((csfc.length()-4),csfc.length());			
+				 clastthree = csfc.substring((csfc.length()-3),csfc.length());			
 			csplit = csfc.split("-");
 			seclength = csplit[1].length();
-			assemblyDataPrefix = csplit[0];
 			 } catch (Exception e) {
 					throw new InvalidGlassSerialNumberException(20119, sfcNumber);
 			 }
-			if (sfcPrefix.length()==2 && assemblyDataPrefix.length()==2){
-				if (sfcPrefix.equals("UR")){
-					if (!assemblyDataPrefix.equals("FM")){
-						throw new InvalidGlassSerialNumberException(20120,sfcNumber);
-					}
-				} else if (sfcPrefix.equals("TR")){
-					if (!assemblyDataPrefix.equals("UR")){
-						throw new InvalidGlassSerialNumberException(20121,sfcNumber);
-					}
-				} else {
-					throw new InvalidGlassSerialNumberException(20119,sfcNumber);
-				}
-				
-			} else {
-				throw new InvalidGlassSerialNumberException(20119, sfcNumber);
-			}
-	       
 	        if( (csplit.length>1) && (seclength>3)){
-	   	        if (!clastthree.contains("-")){
+	   	        if (clastthree.startsWith("-") || !clastthree.contains("-")){
 	        	assemblyData = csfc.substring(3,csfc.length());
-	   	        } else {
-	   	        clastpiece = csfc.substring((csfc.lastIndexOf("-")+1),csfc.length());
-	   	        	if (clastpiece.length() > 2){
-	   	        		throw new InvalidGlassSerialNumberException(20125, csfc);
-	   	        	} else if (clastpiece.length() == 0){
-	   	        		throw new InvalidGlassSerialNumberException(20123, csfc);
-	   	        	} else {	   	        	
-	   	        		assemblyData =  csfc.substring(3,csfc.lastIndexOf("-"));
-	   	        	}
+	   	        } else {	   	        	
+	   	        assemblyData =  csfc.substring(3,csfc.lastIndexOf("-"));
 	   	        }
 	        } else {
 	        	throw new InvalidGlassSerialNumberException(20123, csfc);
@@ -146,10 +109,13 @@ public class ValidateAssemblyFieldsExtensionActivity implements
 				}		
 			}
 		}
-		
+		}
 		}
 		}
 
+	/**
+	 *	Initialization of services that are represented as fields.
+	 */
 	private void initServices(){
 		sfcStateService = Services.getService(COM_SAP_ME_PRODUCTION,SFC_STATE_SERVICE);
 		systemRuleService = Services.getService(COM_SAP_ME_APPCONFIG,SYSTEM_RULE_SERVICE);
